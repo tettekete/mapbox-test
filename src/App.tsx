a2a1ef4d49c98a9e9e57f5a73a2bf575b5f486fb
tettekete <6535634+tettekete@users.noreply.github.com>
2025-09-01 13:52:07 +0900

    fix: 使用していない React の import を削除
    プロダクションビルドがエラーとなるため。

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import MiniMap from "./components/MiniMap";
 import TsunamiHazardMap from "./components/TsunamiHazardMap";
 import MultiLayerMap from "./components/MultiLayerMap";
